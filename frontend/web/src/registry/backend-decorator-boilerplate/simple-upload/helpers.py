75b49a6309840cfa49da1027087f259ff51d45ee
mdwbr <mdwbr@users.noreply.github.com>
2025-11-21 16:55:49 +0000

    Add helpers.py, service.yml, validator-report-static.sol

---
+++ frontend/web/src/registry/backend-decorator-boilerplate/simple-upload/helpers.py
@@ -0,0 +1,4 @@
+// Auto-generated file: /Users/kevinguoquan/Desktop/zama备用文件/测试/Zama项目/collabedit_fhe/frontend/web/src/registry/backend-decorator-boilerplate/simple-upload/helpers.py
+// Generated at: 2025-11-27T15:10:54.121Z
+
+
